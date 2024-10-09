edfc24332a588d3e07d09a9d27505c1b96f8ea6a
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-10-09 15:41:51 +0600

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import React from 'react'
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
-import HomePage from './pages/HomePage';
-import JobsPage from './pages/JobsPage';
-import JobPage from './pages/JobPage';
-import AddJobPage from './pages/AddJobPage';
-import EditJobPage from './pages/EditJobPage';
+// App.js
+import React from 'react';
+import Counter from './Counter'; // Import the Counter component
 
-const App = () => {
-
-
-
-  
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<MainLayout />} >
-        <Route index element={<HomePage />} />
-        <Route path='/jobs' element={<JobsPage />}/>
-        <Route path='jobs/1' element={<JobPage />}/>
-        <Route path='edit-job/2' element={<EditJobPage/>}/>
-        <Route path='/add-job' element={<AddJobPage/>}/>
-      </Route>
-    )
-  );
-
+function App() {
   return (
-    <RouterProvider router={router} />
-  )
+    <div>
+      <Counter /> {/* Use the Counter component */}
+    </div>
+  );
 }
 
-export default App
+export default App;
