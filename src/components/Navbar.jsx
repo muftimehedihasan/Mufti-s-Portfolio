313199a92ca795d27126bc86534c9f3c20ccd8c4
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-10-09 21:30:25 +0600

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import logo from "../assets/kevinRushLogo.png"
+import { FaLinkedin } from 'react-icons/fa6'
+import { FaGithub } from 'react-icons/fa6'
+import { FaFacebook } from 'react-icons/fa6'
+
 
 const Navbar = () => {
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
-        hello
+
+        <div className='flex flex-shrink-0 items-center'>
+        <img className='mx-2 w-10' src={logo} alt="" />
+        </div>
+
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin />
+        <FaGithub /> 
+        <FaFacebook/>
+        </div>
+
     </nav>
   )
 }
