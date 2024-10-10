7440c6dc852213e3778659e7795bb6f9ac012812
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-10-10 19:12:46 +0600

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,10 @@ const Navbar = () => {
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
   <div className='flex flex-shrink-0 items-center flex-col'>
-    <img className='mx-2 w-30' src={logo} alt="Logo" />
-    <hr className='w-2/4 w-full border h-[1.5px] bg-white mt-2' /> 
-  </div>
+  <img className='mx-2 w-30' src={logo} alt="Logo" />
+  <hr className='w-1/2 border h-[1.5px] bg-white mt-2' />
+</div>
+
 
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
         <a href='https://www.linkedin.com/in/muftimehedihasan' target='_blank' rel='noopener noreferrer'>
