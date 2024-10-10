0df6dddf88b545c49b2b6ef1dfd0038ec131d561
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-10-10 18:46:07 +0600

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
     <nav className='mb-20 flex items-center justify-between py-6'>
       <div className='flex flex-shrink-0 items-center'>
         <img className='mx-2 w-30' src={logo} alt="Logo" />
+        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
       </div>
 
       <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
