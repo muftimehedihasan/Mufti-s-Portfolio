5cd1ed38e2fb3fc29df1aa9875a985ee21249856
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-10-10 15:59:27 +0600

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/Logo.png"
 import { FaLinkedin } from 'react-icons/fa6'
 import { FaGithub } from 'react-icons/fa6'
 import { FaFacebook } from 'react-icons/fa6'
+import { NavLink } from 'react-router-dom';
 
 
 const Navbar = () => {
@@ -14,10 +15,18 @@ const Navbar = () => {
         </div>
 
         <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <FaLinkedin />
-        <FaGithub /> 
-        <FaFacebook/>
-        </div>
+  <NavLink href='linkedin.com/in/ muftimehedihasan' target='_blank' rel='noopener noreferrer'>
+    <FaLinkedin />
+  </NavLink>
+  <NavLink href='https://github.com/muftimehedihasan' target='_blank' rel='noopener noreferrer'>
+    <FaGithub />
+  </NavLink>
+  <NavLink href='https://www.facebook.com/muftimehedihasan?mibextid=ZbWKwL
+' target='_blank' rel='noopener noreferrer'>
+    <FaFacebook />
+  </NavLink>
+</div>
+
 
     </nav>
   )
