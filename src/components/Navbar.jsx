f33c380dadc0ce39a8c2a67ebd19482a5576d342
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-10-10 16:25:02 +0600

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,29 @@
 import React from 'react'
 import logo from "../assets/Logo.png"
-import { FaLinkedin } from 'react-icons/fa6'
-import { FaGithub } from 'react-icons/fa6'
-import { FaFacebook } from 'react-icons/fa6'
-import { NavLink } from 'react-router-dom';
-
+import { FaLinkedin, FaGithub, FaFacebook } from 'react-icons/fa6';
 
 const Navbar = () => {
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+        <img className='mx-2 w-30' src={logo} alt="Logo" />
+      </div>
 
-        <div className='flex flex-shrink-0 items-center'>
-        <img className='mx-2 w-30' src={logo} alt="" />
-        </div>
-
-        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-          
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
         <a href='https://www.linkedin.com/in/muftimehedihasan' target='_blank' rel='noopener noreferrer'>
-        <FaLinkedin to='https://www.linkedin.com/in/muftimehedihasan' target='_blank' rel='noopener noreferrer'/>
+          <FaLinkedin />
         </a>
         
-        <NavLink href='https://github.com/muftimehedihasan' target='_blank' rel='noopener noreferrer'>
-        <FaGithub />
-        </NavLink>
-       
-        <NavLink href='https://www.facebook.com/muftimehedihasan?mibextid=ZbWKwL' target='_blank' rel='noopener noreferrer'>
-        <FaFacebook />
-        </NavLink>
-</div>
+        <a href='https://github.com/muftimehedihasan' target='_blank' rel='noopener noreferrer'>
+          <FaGithub />
+        </a>
 
-
+        <a href='https://www.facebook.com/muftimehedihasan?mibextid=ZbWKwL' target='_blank' rel='noopener noreferrer'>
+          <FaFacebook />
+        </a>
+      </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
