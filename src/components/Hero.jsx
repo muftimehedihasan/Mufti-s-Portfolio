bc4b7b0e3ce6ed33f986617fe50ab3a7a6cf6901
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-10-10 17:22:55 +0600

    new commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,9 +9,9 @@ const Hero = () => {
 
             <div className='w-full lg:w-1/2'>
             <div className='flex flex-col items-center lg:items-start'>
-                <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>Mufti Mehedi Hasan
+                <h1 className='pb-16 text-6xl font-thin lg:mt-16 lg:text-6xl'>Mufti Mehedi Hasan
                 </h1>
-                <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</span>
+                <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl text-transparent'>Full Stack Developer</span>
                 <span>
                     <p className='my-2 max-w-xl py-6 font-normal'>{HERO_CONTENT}</p>
                 </span>
