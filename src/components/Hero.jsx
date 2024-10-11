f74ef557025675c47ecdbe0458f68c511a7e95ad
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-10-11 18:20:05 +0600

    new commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import {HERO_CONTENT} from '../constants'
 import profilePic from "../assets/prof.png"
+import { motion } from "framer-motion"
+
+const container = (delay) => ({
+    hidden: {x: -100, opacity: 0},
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: {duration: 0.5, delay: delay}
+    }
+})
 
 const Hero = () => {
   return (
@@ -9,11 +19,24 @@ const Hero = () => {
 
             <div className='w-full lg:w-1/2'>
             <div className='flex flex-col items-center lg:items-start'>
-                <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>Mufti Mehedi Hasan
-                </h1>
-                <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</span>
+                <motion.h1 
+                variants={container(0)}
+                initial="hidden"
+                animate="visible"
+
+                className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-6xl'>Mufti Mehedi Hasan
+                </motion.h1>
+                <motion.span 
+                variants={container(0)}
+                initial="hidden"
+                animate="visible"
+                className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</motion.span>
                 <span>
-                    <p className='my-2 max-w-xl py-6 font-normal'>{HERO_CONTENT}</p>
+                    <motion.p 
+                     variants={container(0)}
+                     initial="hidden"
+                     animate="visible"
+                    className='my-2 max-w-xl py-6 font-normal'>{HERO_CONTENT}</motion.p>
                 </span>
             </div>
             </div>
